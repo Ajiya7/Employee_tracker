bf58f6e9254793c14231d56321fe4a3d2c1e23f8
Faruk Ajiya <Ajiyafaruk@gmail.com>
2022-12-17 00:30:54 -0800

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE Role (
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL NOT NULL,
     department_id INT, 
-    PRIMARY KEY (id)  ,
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id) 
     REFERENCES Department(id)
     ON DELETE SET NULL
