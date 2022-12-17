13f5b2ef0df48160e2740ff0476373d22349dfae
Faruk Ajiya <Ajiyafaruk@gmail.com>
2022-12-17 00:30:11 -0800

    created database and tables in the schema

---
+++ db/schema.sql
@@ -0,0 +1,41 @@
+-- Drops the Employee_db if it exists currently --
+DROP DATABASE IF EXISTS Employee_db;
+-- Creates the Employee_db database --
+CREATE DATABASE Employee_db;
+-- use Employee_db database --
+USE Employee_db; 
+
+-- Creates the table "Department" within Employee_db --
+CREATE TABLE Department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Creates the table "Role" within Employee_db --
+CREATE TABLE Role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL NOT NULL,
+    department_id INT, 
+    PRIMARY KEY (id)  ,
+    FOREIGN KEY (department_id) 
+    REFERENCES Department(id)
+    ON DELETE SET NULL
+);
+
+-- Creates the table "Employee" within Employee_db --
+CREATE TABLE Employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id
+    REFERENCES Role(id)
+    ON DELETE SET NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) 
+    REFERENCES Employee(id) 
+    ON DELETE SET NULL
+);
